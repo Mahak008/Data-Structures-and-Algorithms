48394c10a24356f0a69b2ae657a0f192303712e1
Mahak <mahakgarg8002@gmail.com>
2023-07-14 10:00:06 +0530

    file

--- Array/School/Sum of Array.cpp
+++ Array/School/Sum of Array.cpp
@@ -1,10 +1,9 @@
 // function to return sum of elements
 // in an array of size n
-	
 int sum(int arr[], int n) {
   int ans = 0;
-	for(int i = 0; i < n; i++) {
-	  ans += arr[i];
-	}
-	return ans;
+  for(int i = 0; i < n; i++) {
+	ans += arr[i];
+  }
+  return ans;
 }
