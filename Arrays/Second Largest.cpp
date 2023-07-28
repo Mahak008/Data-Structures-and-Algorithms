851ad51528114b903d7b9e17ef0c10086fd497be
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-07-28 15:07:40 +0530

    Update Second Largest.cpp

--- Arrays/Second Largest.cpp
+++ Arrays/Second Largest.cpp
@@ -3,17 +3,19 @@
 
 int print2largest(int arr[], int n) {
   set <int> s;
-	for(int i = 0; i < n; i++) {
-	  s.insert(arr[i]);
-	}
+  for(int i = 0; i < n; i++) {
+    s.insert(arr[i]);
+  }
 	    
   auto it = s.begin();
-	int l = s.size();
+  int l = s.size();
         
   for(int i = 0; i < l-2; i++) {
     it++;
   }
+	
   int ans = *it;
+	
   if(s.size() == 1) {
     return -1;
   }
