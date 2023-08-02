96c2280178b48ecc98ba8df3d79749bd56538c8a
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-08-02 14:14:49 +0530

    Update Find Index.cpp

--- Arrays/Find Index.cpp
+++ Arrays/Find Index.cpp
@@ -15,8 +15,7 @@ return ans;
 // N = 6
 // arr[] = { 1, 2, 3, 4, 5, 5 }
 // Key = 5
+
 // Output:  4 5
-// Explanation:
-// 5 appears first time at index 4 and
-// appears last time at index 5.
-// (0 based indexing)
+
+// Explanation: 5 appears first time at index 4 and appears last time at index 5. (0 based indexing)
