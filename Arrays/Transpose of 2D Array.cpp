236430f3544d34764cf558dc50127860f450eb02
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-08-02 14:16:19 +0530

    Update Transpose of 2D Array.cpp

--- Arrays/Transpose of 2D Array.cpp
+++ Arrays/Transpose of 2D Array.cpp
@@ -16,7 +16,7 @@ vector<vector<int>> transpose(int a[][M], int n) {
 
 // Output : 1 4 7 2 5 8 3 6 9
 
-// Explanation:1 2 3       1 4 7
-//             4 5 6 ----> 2 5 8
-//             7 8 9       3 6 9
+// Explanation: 1 2 3       1 4 7
+//              4 5 6 ----> 2 5 8
+//              7 8 9       3 6 9
 // Transpose of array.
