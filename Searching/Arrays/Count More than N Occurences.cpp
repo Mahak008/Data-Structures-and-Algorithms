c56fecf0f2ae408f7a97ba6d64f3f90fcdfc0457
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-08-02 13:42:33 +0530

    Create Count More than N Occurences.cpp

---
+++ Searching/Arrays/Count More than N Occurences.cpp
@@ -0,0 +1,27 @@
+//Function to find all elements in array that appear more than N = n/k times.
+
+int countOccurence(int arr[], int n, int k) {
+  map <int,int> m;
+        
+  for(int i=0;i<n;i++) {
+    m[arr[i]]++;
+  }
+            
+  int count=0;
+        
+  for(auto it: m){
+    if(it.second>(n/k))
+      count++;
+    }
+    return count;
+  }
+}
+
+// Input:
+// N = 8
+// arr[] = {3,1,2,2,1,2,3,3}
+// k = 4
+// Output: 2
+// Explanation: In the given array, 3 and
+//  2 are the only elements that appears 
+// more than n/k times.
