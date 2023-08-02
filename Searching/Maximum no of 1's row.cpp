14a288feb4b85b8ffca27549d2466522cb1036f0
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-08-02 13:40:20 +0530

    Create Maximum no of 1's row.cpp

---
+++ Searching/Maximum no of 1's row.cpp
@@ -0,0 +1,24 @@
+int maxOnes (vector <vector <int>> &Mat, int N, int M) {
+  int max = INT_MIN, x;
+  for(int i = 0; i < N; i++) {
+    int count = 0;
+        
+    for(int j = 0; j < M; j++) {
+      if(Mat[i][j] == 1) {
+        count++;
+      }
+    }
+                
+    if(count > max) {
+      max = count;
+      x = i;
+    }
+  }
+  return x;
+}
+
+// N = 3, M = 4
+// Mat[] = {{0 1 1 1},
+//          {0 0 1 1},
+//          {0 0 1 1}}
+// Output: 0
