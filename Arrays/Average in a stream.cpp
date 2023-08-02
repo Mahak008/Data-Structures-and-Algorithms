667ba8693a62ac3f33fa7652d2bca20dbd8e0e92
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-08-02 14:11:23 +0530

    Update Average in a stream.cpp

--- Arrays/Average in a stream.cpp
+++ Arrays/Average in a stream.cpp
@@ -1,10 +1,23 @@
 vector<float> streamAvg(int arr[], int n) {
   vector <float> v;
-	float ans = 0, avg = 0;
-	for(int i = 0; i < n; i++) {
-	  ans += arr[i];
-	  avg = ans/(i+1);
-	  v.push_back(avg);
-	}
-	return v;
+  float ans = 0, avg = 0;
+    
+  for(int i = 0; i < n; i++) {
+    ans += arr[i];
+    avg = ans/(i+1);
+    v.push_back(avg);
+  }
+  return v;
 }
+
+// Input:
+// n = 5
+// arr[] = {10, 20, 30, 40, 50}
+
+// Output: 10.00 15.00 20.00 25.00 30.00 
+
+// Explanation: 
+// 10 / 1 = 10.00
+// (10 + 20) / 2 = 15.00
+// (10 + 20 + 30) / 3 = 20.00
+// And so on.
