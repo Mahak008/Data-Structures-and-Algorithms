6490a0d667c006e610ce41ff09c24041805a4ac3
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-12-30 20:15:30 +0530

    Update P8.cpp

--- Patterns/P8.cpp
+++ Patterns/P8.cpp
@@ -13,3 +13,19 @@ for(int i = 1; i <= n; i++) {
 // 345
 // 4567
 // 56789
+
+int n = 5;
+for(int i = 1; i <= n; i++) {
+    int val = i;
+    for (int j = 1; j <= i; j++) {
+        cout << val--;
+    }
+    cout << endl;
+}
+
+// Output - 
+// 1
+// 21
+// 321
+// 4321
+// 54321
