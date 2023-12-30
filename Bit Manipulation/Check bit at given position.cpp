04564f352361184f47b42b5e8fb66d00835e66ef
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-12-30 22:57:34 +0530

    Create Check bit at given position.cpp

---
+++ Bit Manipulation/Check bit at given position.cpp
@@ -0,0 +1,22 @@
+int n = 5, k = 2;
+int n1 = n >> (k-1);
+if(n1 & 1) {
+  cout << '1';
+}
+else {
+  cout << '0';
+}
+
+// Another method
+int n = 5, k = 3;
+int n1 = 1 << (k-1);
+if(n1 & n) {
+  cout << '1';
+}
+else {
+  cout << '0';
+}
+
+// Output - 5(101)
+// 0
+
