5380cf4585994fd1178de930aa873a62b62cddda
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-04 20:50:05 +0530

    Create Xor of no. from 1 to n.cpp

---
+++ Bit Manipulation/Xor of no. from 1 to n.cpp
@@ -0,0 +1,16 @@
+int n = 9;
+if(n % 4 == 0) {
+  cout << n;
+}
+else if(n % 4 == 1) {
+  cout << 1;
+}
+else if(n % 4 == 2) {
+  cout << n+1;
+}
+else if(n % 4 == 3) {
+  cout << 0;
+}
+
+// Output -
+// 1
