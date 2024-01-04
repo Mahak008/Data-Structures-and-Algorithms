0e9ac3512b1dd24bfb6b287322c72ee86ce74be6
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-04 12:19:56 +0530

    Update Number of 1 Bits.cpp

--- Bit Manipulation/Number of 1 Bits.cpp
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -1,9 +1,9 @@
-int numSetBits(unsigned int A) {
-    int count = 0;
-    while(A) {
-        A = A & (A-1);
-        count++;
-    }
-    return count;
-}
+int A = 4;
+int count = 0;
+while(A) {
+    A = A & (A-1);
+    count++;
+}    
+cout <<  count;
 
+// Output - 1
