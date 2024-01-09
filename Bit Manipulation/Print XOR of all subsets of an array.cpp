9746c19ff4fc1509e73772a8f08d07785cc9b215
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-09 10:35:40 +0530

    Update Print XOR of all subsets of an array.cpp

--- Bit Manipulation/Print XOR of all subsets of an array.cpp
+++ Bit Manipulation/Print XOR of all subsets of an array.cpp
@@ -1,7 +1,6 @@
+// Answer will always be 0 because every number in the set will repeat even no. of times while creating the subsets. 
 if (n == 1)
+  return arr[0];
 
-        return Set[0];
-
-    else
-
-        return 0;
+else
+  return 0;
