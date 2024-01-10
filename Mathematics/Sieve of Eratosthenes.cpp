f75ee58222080751410936ece190ce7b173cf0cf
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-10 23:04:48 +0530

    Create Sieve of Eratosthenes.cpp

---
+++ Mathematics/Sieve of Eratosthenes.cpp
@@ -0,0 +1,22 @@
+int n = 20;
+
+vector <bool> v(n+1, true);
+
+v[0] = v[1] = false;
+
+for(int i = 2; i < n; i++) {
+  if(v[i]) {
+    for(int j = 2*i; j < n; j+=i) { 
+      v[j] = 0;
+    }
+  }
+}
+
+for (int p = 2; p < n; p++) {
+  if (v[p]) {
+    cout << p << " ";
+  }
+}
+
+// Output - 
+// 2 3 5 7 11 13 17 19 
