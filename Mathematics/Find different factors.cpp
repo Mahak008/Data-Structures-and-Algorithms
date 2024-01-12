9721db4edf56aeae67009a483ebbc4564d729757
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-12 22:37:46 +0530

    Update Find different factors.cpp

--- Mathematics/Find different factors.cpp
+++ Mathematics/Find different factors.cpp
@@ -1,6 +1,6 @@
 int n = 88;
 int a = 1, b = 1, c = 1;
-for(int i = 2;i*i<=n;i++) {
+for(int i = 2; i * i <= n; i++) {
     if(n % i == 0) {
         a = i;
         break;
@@ -8,9 +8,9 @@ for(int i = 2;i*i<=n;i++) {
 }
 
 n = n / a;
-for(int i = 2; i*i<n; i++) {
-    if(n % i == 0) {
-    
+
+for(int i = 2; i * i <= n; i++) {
+    if(n % i == 0) {    
         if(i != a) {
             b = i;
         }
