15878c19987200bb8d4af265fb3d16918a8962d1
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-19 03:30:12 +0530

    Create Segmented Sieve.cpp

---
+++ Mathematics/Segmented Sieve.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N = 1000000;
+bool sieve [N + 1];
+
+void createSieve() {
+  for(int i = 2; i <= N;i++) {
+    sieve [i] = true;
+  }
+
+  for(int i = 2; i*i <= N;i++) {
+    if (sieve [i] == true) {
+      for(int j = i*i; j <= N; j+=i) {
+        sieve [j] = false;
+      }
+    }
+  }
+}
+
+vector<int> generatePrimes (int N) {
+  vector <int> ds;
+  for(int i = 2; i <= N; i++) {
+    if(sieve[i] == true) {
+      ds.push_back(i);
+    }
+  }
+}
+
+int main() {
+  createSieve();
+  // step 1: generate all primes till sqrt(r)
+}
