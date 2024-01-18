8600c0d2b1f98501c23b41e431790ad2b3b0dbf3
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-19 03:00:48 +0530

    Update n is the smallest prime factor.cpp

--- Mathematics/n is the smallest prime factor.cpp
+++ Mathematics/n is the smallest prime factor.cpp
@@ -1,18 +1,21 @@
 // n is the smallest prime factor of how many numbers in a range
-int n = 50, r = 2;
+int n = 20, r = 3;
 vector <int> v(n+1, 1);
 v[0] = v[1] = 0;
 
 for(int i = 2; i*i <= n; i++) {
   if(v[i] == 1) {
     for(int j = i*i; j <= n; j+=i) {
-      v[i]++;
-      v[j] = 0;
+      if(v[j] != 0) {
+        v[i]++;
+        v[j] = 0;
+      }
     }
   }
 }
 cout << v[r];
 
+
 // Output -
-// 25
-// 2 is the smallest prime factor of 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50
+// 3
+// 2 is the smallest prime factor of 3 6, 15
