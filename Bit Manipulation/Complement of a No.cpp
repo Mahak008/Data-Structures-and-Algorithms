92913606705591888f84b37e3c321c9ec77747de
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-25 16:26:38 +0530

    Update Complement of a No.cpp

--- Bit Manipulation/Complement of a No.cpp
+++ Bit Manipulation/Complement of a No.cpp
@@ -4,4 +4,15 @@ int mask = (1 << dig) - 1;
 int res  = (mask ^ n);
 cout res;
 
+// Alternative Approach
+
+int n = 10, mask = 0;
+int m = n;
+while(m > 0) {
+    mask = (mask << 1) | 1;
+     m = m >> 1;
+}
+int res  = (mask ^ n);
+cout << mask << endl << res;
+
 // Output - 5
