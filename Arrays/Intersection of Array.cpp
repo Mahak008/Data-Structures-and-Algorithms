849f48e33da48c149cd26a9bb2c4be932f51e63f
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-26 15:49:42 +0530

    Update Intersection of Array.cpp

--- Arrays/Intersection of Array.cpp
+++ Arrays/Intersection of Array.cpp
@@ -22,6 +22,32 @@ for(auto i : ans) {
     cout << i << endl;
 }
 
+// Optimized Approach
+
+vector <int> v1{1, 2, 3, 4, 40};
+vector <int> v2{ 3, 4, 4, 8};
+vector <int> ans;
+unsigned i = 0, j = 0;
+
+while(i < v1.size() && j < v2.size()) {
+    if(v1[i] < v2[j]) {
+      i++;
+    }
+
+    else if(v1[i] == v2[j]) {
+      ans.push_back(v1[i]);
+      i++;
+      j++;
+    }
+
+    else {
+        j++;
+    }
+}
+for(auto it : ans) {
+    cout << it << endl;
+}
+
 // Output -
 // 1
 // 2
