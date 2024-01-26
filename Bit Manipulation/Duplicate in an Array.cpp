ec2f2c4e547d94de7c3d1087f29dce8d74069c64
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-26 16:01:46 +0530

    Update Duplicate in an Array.cpp

--- Bit Manipulation/Duplicate in an Array.cpp
+++ Bit Manipulation/Duplicate in an Array.cpp
@@ -1,1 +1,15 @@
+// Only if the element is in consecutive range and repeats twice
 
+vector <int> v1{1, 2, 3, 4, 4};
+int ans = 0;
+
+for(unsigned i = 0; i < v1.size(); i++) {
+    ans ^= v1[i];
+}
+
+for(unsigned i = 1; i < v1.size(); i++) {
+    ans ^= i;
+}
+cout << ans;
+
+// Output - 4
