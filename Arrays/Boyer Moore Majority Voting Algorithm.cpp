05698bcc480ee59978ec9392f704ef210de64b52
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-27 01:20:54 +0530

    Create Boyer Moore Majority Voting Algorithm.cpp

---
+++ Arrays/Boyer Moore Majority Voting Algorithm.cpp
@@ -0,0 +1,24 @@
+int candidate = 0, count = 0;
+int a[] = { -2, 1, -3, 4, 1, -2, 1, 5, -3, 1, 1 };
+int n = sizeof(a)/sizeof(a[0]);
+
+for(int i = 0; i < n; i++) {
+    if(count == 0) {
+        candidate = a[i];
+    }
+    else {
+        if(a[i] == candidate)
+            count++;
+        else
+            count--;
+    }
+}
+int counter =  0;
+for(int i = 0; i < n; i++) {
+    if(a[i] == candidate) {
+        counter++;
+    }
+}
+if(counter >= n/2) {
+    cout << candidate;
+}
