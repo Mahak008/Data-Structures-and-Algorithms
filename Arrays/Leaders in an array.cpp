45dae77221d41587e3fb7906562d0755865bd12e
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-10 02:31:39 +0530

    Update Leaders in an array.cpp

--- Arrays/Leaders in an array.cpp
+++ Arrays/Leaders in an array.cpp
@@ -1,1 +1,22 @@
+vector<int> superiorElements(vector<int>&a) {
+    vector <int> v;
+    int n = a.size();
+    v.push_back(a[n-1]);
+    for(int i = 0; i < n-1; i++) {
+        bool leader = true;
+        for(int j = i+1; j < n; j++) {
+            if(a[i] <= a[j]) {
+                leader = false;
+                break;
+            }
+        }
+        if(leader == true) {
+            v.push_back(a[i]);
+        }
+    }
+    reverse(v.begin()+1, v.end());
+    return v;
+}
 
+// Input : 5 4 3
+// Output : 3 4 5 
