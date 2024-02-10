25d535a9ecc4990f0d18cf30a475f99d3a9da636
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-11 01:59:58 +0530

    Update Decimal to Binary.cpp

--- Bit Manipulation/Decimal to Binary.cpp
+++ Bit Manipulation/Decimal to Binary.cpp
@@ -8,3 +8,18 @@ while(n > 0) {
 cout << ans;
 
 // Output - 10100
+
+
+// Binary to Decimal
+int n = 110, ans = 0, i = 0, bit;
+while(n > 0) {
+  bit =  n % 10;
+  if(bit == 1) {
+    ans = bit * pow(2,i) + ans;
+  }
+  n /= 10;
+  i++;
+}
+cout << ans;
+
+// Output - 20 
