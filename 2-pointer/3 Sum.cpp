b19c8d8fcba8f58797f12f87a117df8ba58636f0
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-11 17:56:05 +0530

    Update 3 Sum.cpp

--- 2-pointer/3 Sum.cpp
+++ 2-pointer/3 Sum.cpp
@@ -1,1 +1,38 @@
+vector<vector<int>> threeSum(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+  vector<vector<int>> ans;
+  for(int i = 0; i < nums.size(); i++) {
+    if(i > 0 && nums[i] == nums[i-1]) {
+      continue;
+    }
+    int j = i+1;
+    int k = nums.size()-1;
+    while(j < k) {
+      int sum = nums[i] + nums[j] + nums[k];
+      if(sum < 0) {
+        j++;
+      }
+      else if(sum > 0) {
+        k--;
+      }
+      else {
+        vector <int> num = {nums[i], nums[j], nums[k]};
+        ans.push_back(num);
+        j++;
+        k--;
+        while(j < k && nums[j] == nums[j-1]) {
+          j++;
+        }
+        while(j < k && nums[k] == nums[k+1]) {
+          k--;
+        }
+      }
+    }
+  }
+  return ans;
+}
 
+// Input : -1,0,1,2,-1,-4
+// Output : 
+// -1,-1,2
+// -1,0,1
