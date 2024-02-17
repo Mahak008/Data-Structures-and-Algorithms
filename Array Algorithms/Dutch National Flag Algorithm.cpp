d5fecd442f55fb30ab4887c58b6167d95d60d805
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-17 15:40:15 +0530

    Update Dutch National Flag Algorithm.cpp

--- Array Algorithms/Dutch National Flag Algorithm.cpp
+++ Array Algorithms/Dutch National Flag Algorithm.cpp
@@ -1,2 +1,17 @@
 // Sort an array of 0s, 1s and 2s
 
+void sortArray(vector<int> &arr, int n) {
+  int low = 0, mid = 0, high = n - 1;
+  while (mid <= high) {
+    if (arr[mid] == 0) {
+      swap(arr[low], arr[mid]);
+      low++;
+      mid++;
+    } else if (arr[mid] == 1) {
+      mid++;
+    } else {
+      swap(arr[high], arr[mid]);
+      high--;
+    }
+  }
+}
