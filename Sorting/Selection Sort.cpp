1c641829182d905b504ef65e00a2df45549f20c6
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-28 18:49:04 +0530

    Update and rename s1.cpp to Selection Sort.cpp

---
+++ Sorting/Selection Sort.cpp
@@ -0,0 +1,18 @@
+int arr[] = {13, 46, 24, 52, 20, 9};
+int n = sizeof(arr)/sizeof(arr[0]);
+
+for(int i = 0; i <= n-2; i++) {
+  int mini = i;
+  for(int j = i; j <= n-1; j++) {
+    if(arr[j] < arr[mini]) {
+      mini = j;
+    }
+  }
+  swap(arr[mini], arr[i]);
+}
+
+// 9 46 24 52 20 13 
+// 9 13 24 52 20 46 
+// 9 13 20 52 24 46 
+// 9 13 20 24 52 46 
+// 9 13 20 24 46 52 
