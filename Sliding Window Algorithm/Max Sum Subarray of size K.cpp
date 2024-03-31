1870177d7ec19c28f4433a2c8ffb787ed3fb515f
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-04-01 00:01:25 +0530

    Update Max Sum Subarray of size K.cpp

--- Sliding Window Algorithm/Max Sum Subarray of size K.cpp
+++ Sliding Window Algorithm/Max Sum Subarray of size K.cpp
@@ -1,20 +1,24 @@
 long maximumSumSubarray(int K, vector<int> &Arr , int N){
-        // code here 
-        long Max = 0;
-        long sum = 0;
-        int i = 0, j ;
+  long Max = 0;
+  long sum = 0;
+  int i = 0, j ;
+
+  for (j = 0; j < K; ++j) {
+    sum += Arr[j];
+  }
+  Max = sum;
     
-        // Calculate sum of first K elements
-        for (j = 0; j < K; ++j) {
-            sum += Arr[j];
-        }
-        Max = sum;
-    
-        // Slide the window and update sum and Max
-        while (j < N) {
-            sum += Arr[j] - Arr[i];
-            Max = max(Max, sum);
-            ++i;
-            ++j;
-        }
-        return Max;
+  while (j < N) {
+    sum += Arr[j] - Arr[i];
+    Max = max(Max, sum);
+    ++i;
+    ++j;
+  }
+  return Max;
+}
+
+// Input:
+// N = 4, K = 2
+// Arr = [100, 200, 300, 400]
+// Output:
+// 700
