3617770f252be33267bae1141ddb286ea4d09e46
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-04-06 18:44:48 +0530

    Update Fruits and Baskets.cpp

--- Sliding Window Algorithm/Fruits and Baskets.cpp
+++ Sliding Window Algorithm/Fruits and Baskets.cpp
@@ -1,1 +1,18 @@
-
+int findMaxFruits(vector<int> &arr, int n) {
+    // Write your code here.
+    int l = 0, r = 0, maxLen = 0;
+    map <int, int> mp;
+    while(r < n) {
+        ++mp[arr[r]];
+        if(mp.size() > 2) {
+            --mp[arr[l]];
+            if(mp[arr[l]] == 0) {
+                mp.erase(arr[l]);
+            }
+            ++l;
+        }
+        maxLen = max(maxLen, (r-l+1));
+        ++r;
+    }
+    return maxLen;
+}
