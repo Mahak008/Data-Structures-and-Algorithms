ae422f2f39ce8a93eaef05d13e80d5e665a8acca
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-04-16 18:21:37 +0530

    Update Capacity To Ship Packages Within D Days.cpp

--- Binary Search/Capacity To Ship Packages Within D Days.cpp
+++ Binary Search/Capacity To Ship Packages Within D Days.cpp
@@ -30,3 +30,12 @@ int leastWeightCapacity(vector<int> &weights, int days) {
   }
   return low;
 }
+
+// Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+// Output: 15
+// Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+// 1st day: 1, 2, 3, 4, 5
+// 2nd day: 6, 7
+// 3rd day: 8
+// 4th day: 9
+// 5th day: 10
