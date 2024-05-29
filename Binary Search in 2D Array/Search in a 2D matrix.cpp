8bef251827ca167e95865ff930f4c659a39cb3a6
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-05-29 11:24:17 +0530

    Update Search in a 2D matrix.cpp

--- Binary Search in 2D Array/Search in a 2D matrix.cpp
+++ Binary Search in 2D Array/Search in a 2D matrix.cpp
@@ -1,3 +1,6 @@
+// Each row is sorted in increasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
   if(matrix.empty()) {
     return false;
@@ -24,3 +27,11 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
   }
   return false;
 }
+
+// Input: 
+//  1  3  5  7
+// 10 11 16 20
+// 23 30 34 60
+  
+// target = 3
+// Output: true
