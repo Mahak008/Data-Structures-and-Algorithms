a65dcad280f7cf116646d508b4ecb6a8188a916f
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-05-29 11:26:08 +0530

    Update Search a 2D Matrix II.cpp

--- Binary Search in 2D Array/Search a 2D Matrix II.cpp
+++ Binary Search in 2D Array/Search a 2D Matrix II.cpp
@@ -18,3 +18,13 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
   }
   return false;
 }
+
+// Input: 
+//  1  4  7 11 15
+//  2  5  8 12 19
+//  3  6  9 16 22
+// 10 13 14 17 24
+// 18 21 23 26 30
+  
+// target = 5
+// Output: true
