491b0b38c452a5708928af3456975579f776e25a
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-01 12:33:33 +0530

    Update Postfix Evaluation.cpp

--- Stack/Postfix Evaluation.cpp
+++ Stack/Postfix Evaluation.cpp
@@ -1,1 +1,42 @@
+// Algorithm
+// 1. If the element is a number, push it into the stack.
+// 2. If the element is an operator, pop operands for the operator from the stack. 
+// 3. Evaluate the operator and push the result back to the stack.
+// 4. When the expression is ended, the number in the stack is the final answer.
 
+int evaluatePostfix(string S) {
+        stack<int> s;
+        int len = S.length();
+        for(int i = 0; i < len; i++) {
+            if(isdigit(S[i])) {
+                s.push(S[i] - '0');
+            }
+            else {
+                int op1 = s.top();
+                s.pop();
+                int op2 = s.top();
+                s.pop();
+                switch(S[i]) {
+                    case '+' :
+                        s.push(op2 + op1);
+                        break;
+                    case '-' :
+                        s.push(op2 - op1);
+                        break;
+                    case '*' :
+                        s.push(op2 * op1);
+                        break;
+                    case '/' :
+                        s.push(op2 / op1);
+                        break;
+                    case '%' :
+                        s.push(op2 % op1);
+                        break;
+                    case '^':
+                        s.push(pow(op2, op1));
+                        break;
+                }
+            }
+        }
+        return s.top();
+    }
