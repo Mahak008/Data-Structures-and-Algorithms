18223cb3d6d05981abebd08f5a4c30a03f2141e1
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-01 11:42:57 +0530

    Update Prefix to Infix.cpp

--- Stack/Prefix to Infix.cpp
+++ Stack/Prefix to Infix.cpp
@@ -1,11 +1,9 @@
-// Read the Prefix expression in reverse order (from right to left)
-// If the symbol is an operand, then push it onto the Stack
-// If the symbol is an operator, then pop two operands from the Stack 
-// Create a string by concatenating the two operands and the operator between them. 
-// string = (operand1 + operator + operand2) 
-// And push the resultant string back to Stack
-// Repeat the above steps until the end of Prefix expression.
-// At the end stack will have only 1 string i.e resultant string
+// Algorithm
+// 1. Read the Prefix expression in reverse order (from right to left)
+// 2. If the symbol is an operand, then push it onto the Stack
+// 3. If the symbol is an operator, then pop two operands from the Stack 
+// 4. Create a string by concatenating the two operands and the operator between them, string = (operand1 + operator + operand2) 
+// 5. Push the resultant string back to Stack
 
 
 class Solution {
