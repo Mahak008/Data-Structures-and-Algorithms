322ab061f9e201756f260ac4e703e225814b15e7
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-01 12:48:16 +0530

    Update Postfix to Prefix.cpp

--- Stack/Postfix to Prefix.cpp
+++ Stack/Postfix to Prefix.cpp
@@ -25,13 +25,14 @@ string postToPre(string post_exp) {
   for (int i = 0; i < len; i++) {
     if (!isOperator(post_exp[i])) {
       s.push(string(1, post_exp[i]));
-    } else {
-      string s1 = s.top();
+    } 
+    else {
+      string op1 = s.top();
       s.pop();
-      string s2 = s.top();
+      string op2 = s.top();
       s.pop();
 
-      string ans = post_exp[i] + s2 + s1;
+      string ans = post_exp[i] + op2 + op1;
       s.push(ans);
     }
   }
