fb486c898829a0f305e1809a1dacc2a624772ec5
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-04 21:57:47 +0530

    Update Nearest Smaller Element to Right.cpp

--- Stack/Nearest Smaller Element to Right.cpp
+++ Stack/Nearest Smaller Element to Right.cpp
@@ -1,1 +1,21 @@
+vector<int> help_classmate(vector<int> arr, int n) { 
+  stack <int> st;
+  vector <int> v;
+  for(int i = n-1; i >= 0; i--) {
+    while(!st.empty() && st.top() >= arr[i]) {
+      st.pop();
+    }
+    if(st.empty()) {
+      v.emplace_back(-1);
+    }
+    else {
+      v.emplace_back(st.top());
+    }
+    st.push(arr[i]);
+  }
+  reverse(v.begin(), v.end());
+  return v;
+} 
 
+// Input: 3, 8, 5, 2, 25
+// Output: 2 5 2 -1 -1
