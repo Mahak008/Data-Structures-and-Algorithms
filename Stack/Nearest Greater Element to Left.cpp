4eee44c8d2b146d3ace26f21de2fa9d754a83069
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-04 22:00:27 +0530

    Update Nearest Greater Element to Left.cpp

--- Stack/Nearest Greater Element to Left.cpp
+++ Stack/Nearest Greater Element to Left.cpp
@@ -1,1 +1,17 @@
+for(int i = 0; i < n ; i++) {
+    while(!st.empty() && st.top () <= a[i]) {
+      st.pop();
+    }
+    if(st.empty()) {
+      v.push_back(-1);
+    }
+    else {
+      v.push_back(st.top());
+    }
+    st.push(a[i]);
+  }
+  return v;
+}
 
+// Input: 4 8 5 2 25
+// Output: -1 -1 8 5 -1
