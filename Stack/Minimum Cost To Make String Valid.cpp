e902cc44155a0f74f6f2153649d8c73cc30b8066
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-07 22:08:52 +0530

    Create Minimum Cost To Make String Valid.cpp

---
+++ Stack/Minimum Cost To Make String Valid.cpp
@@ -0,0 +1,31 @@
+int findMinimumCost(string str) {
+  if (str.length() % 2 != 0) {
+    return -1;
+  }
+
+  stack<char> st;
+  for (int i = 0; i < str.length(); i++) {
+    if (st.empty() || (!st.empty() && str[i] == '{')) {
+      st.push(str[i]);
+    } else {
+      if (st.top() == '{') {
+        st.pop();
+      } else {
+        st.push(str[i]);
+      }
+    }
+  }
+
+  int a = 0, b = 0;
+  while (!st.empty()) {
+    if (st.top() == '{') {
+      ++a;
+    } else {
+      ++b;
+    }
+    st.pop();
+  }
+
+  int ans = (a + 1) / 2 + (b + 1) / 2;
+  return ans;
+}
