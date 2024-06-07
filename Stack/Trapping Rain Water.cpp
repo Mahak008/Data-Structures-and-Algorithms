1c04b98552265604323b44330e3f781ee652da8d
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-07 12:53:23 +0530

    Update Trapping Rain Water.cpp

--- Stack/Trapping Rain Water.cpp
+++ Stack/Trapping Rain Water.cpp
@@ -2,22 +2,22 @@ int trap(vector<int>& height) {
     int n = height.size();
     if (n == 0) return 0;
 
-    stack<int> st;
+    stack <pair<int, int>> st;
     int water = 0;
 
     for (int i = 0; i < n; ++i) {
         // While the current height is greater than the height at the top of the stack
-        while (!st.empty() && height[i] > height[st.top()]) {
-            int top = st.top();
+        while (!st.empty() && st.top().first < height[i]) {
+            int top = st.top().second;
             st.pop();
 
             if (st.empty()) break;
 
-            int distance = i - st.top() - 1;
-            int bounded_height = min(height[i], height[st.top()]) - height[top];
+            int distance = i - st.top().second - 1;
+            int bounded_height = min(height[i], st.top().first) - height[top];
             water += distance * bounded_height;
         }
-        st.push(i);
+        st.push({height[i], i});
     }
 
     return water;
