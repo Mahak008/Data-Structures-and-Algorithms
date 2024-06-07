2c32e5fb761a7a5796c3adb421519fad63846dfd
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-07 23:19:26 +0530

    Update Maximal Rectangle.cpp

--- Stack/Maximal Rectangle.cpp
+++ Stack/Maximal Rectangle.cpp
@@ -52,15 +52,10 @@ int maximalRectangle(vector<vector<char>>& matrix) {
     int n = matrix.size();
     int m = matrix[0].size();
     vector<int> v(m, 0);
+    int maxi = 0;
 
-    for (int j = 0; j < m; j++) {
-        v[j] = matrix[0][j] - '0';
-    }
-
-    int maxi = largestRectangleArea(v);
-
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
             if (matrix[i][j] == '0') {
                 v[j] = 0;
             } else {
