d708a284f0a5142e1b7a6a9399936cca6f1b99cc
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-11 01:25:33 +0530

    Create First non-repeating character in a stream.cpp

---
+++ Queue/First non-repeating character in a stream.cpp
@@ -0,0 +1,25 @@
+string FirstNonRepeating(string A){
+  unordered_map<char, int> mp;
+	queue<char> q;
+	string ans = "";
+	
+  for(int i = 0; i < A.length(); i++) {
+	  mp[A[i]]++;
+		q.push(A[i]);
+		    
+		while(!q.empty() && mp[q.front()] > 1) {
+		  q.pop();
+		}
+		
+    if(q.empty()) {
+		  ans += '#';
+		}
+		else {
+		  ans += q.front();
+		}    
+	}
+	return ans;
+}
+
+// Input: "aabc"
+// Output: "a#bb"
