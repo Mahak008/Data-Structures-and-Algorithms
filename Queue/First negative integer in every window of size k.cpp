46b2f98d2c9810a2bf48fd42cb0dbbd961ced545
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-10 23:30:26 +0530

    Update First negative integer in every window of size k.cpp

--- Queue/First negative integer in every window of size k.cpp
+++ Queue/First negative integer in every window of size k.cpp
@@ -1,29 +1,35 @@
 vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K) {
   queue<long long> q;
   vector<long long> ans;
-  
+
+  // Process first window of size k
   for(int i = 0; i < K; i++) {
     if(A[i] < 0) {
       q.push(i);
     }
   }
-        
+
+  // Store ans of first k size window
   if(q.size() > 0) {
     ans.push_back(A[q.front()]);
   } 
   else {
     ans.push_back(0);
   }
-        
+
+  // Process Remaining Elements
   for(int i = K; i < N; i++) {
+    // Removal
     if(!q.empty() && i - q.front() >= K) {
       q.pop();
     }
-            
+
+    // Addition
     if(A[i] < 0) {
       q.push(i);
     }
-            
+
+    // Push ans to vector
     if(q.size() > 0) {
       ans.push_back(A[q.front()]);
     } 
