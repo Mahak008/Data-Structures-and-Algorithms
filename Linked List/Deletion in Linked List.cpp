60562304010dd44f22e52607d8816ba33e1afe53
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-12 22:03:22 +0530

    Update Deletion in Linked List.cpp

--- Linked List/Deletion in Linked List.cpp
+++ Linked List/Deletion in Linked List.cpp
@@ -1,1 +1,24 @@
-
+Node* deleteNode(Node *head,int x) {
+    Node *temp = head;
+    if (head == NULL) {
+        return NULL;
+    }
+    
+    if (x == 1) {
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    
+    int cnt = 1;
+    while(temp -> next != NULL && cnt != x-1) {
+        temp = temp -> next;
+        cnt++;
+    }
+    
+    Node * del = temp -> next;
+    temp -> next = temp -> next -> next;
+    
+    delete del;
+    return head;
+}
