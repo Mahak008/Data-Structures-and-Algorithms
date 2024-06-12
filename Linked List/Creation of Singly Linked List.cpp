7bf11c0e9a2de2d0522e792b83446350f73eaad0
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-12 21:13:52 +0530

    Update Creation of Singly Linked List.cpp

--- Linked List/Creation of Singly Linked List.cpp
+++ Linked List/Creation of Singly Linked List.cpp
@@ -4,17 +4,15 @@ class Node {
     Node* next;
   
     // Default constructor
-    Node()
-    {
-        data = 0;
-        next = NULL;
+    Node() {
+      data = 0;
+      next = NULL;
     }
   
     // Parameterised Constructor
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
+    Node(int data) {
+      this -> data = data;
+      this -> next = NULL;
     }
 };
 
