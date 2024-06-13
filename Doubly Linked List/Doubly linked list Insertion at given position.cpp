1fb98ca30f3bc4edb041f2cd94e8ec1e20b2a6ae
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-13 13:00:53 +0530

    Update Doubly linked list Insertion at given position.cpp

--- Doubly Linked List/Doubly linked list Insertion at given position.cpp
+++ Doubly Linked List/Doubly linked list Insertion at given position.cpp
@@ -1,19 +1,44 @@
-void addNode(Node *head, int pos, int data) {
-  Node *n = new Node(data);
-  Node *temp = head;
-   
-  int cnt = 0;
-   
-  while(cnt < pos && temp -> next != NULL) {
-    temp = temp -> next;
-    cnt++;
-  }
-   
-  n -> next = temp -> next;
-  if (temp->next != NULL) {
-    temp->next->prev = n;
-  }
-     
-  temp -> next = n;
-  n -> prev = temp;
-}
+class Node {
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+class Solution {
+  public:
+    Node* deleteNode(Node* head, int x) {
+        // If first node is to be deleted
+        if(x == 1) {
+            Node *del = head;
+            head = head -> next;
+            head -> prev = NULL;
+            delete del;
+            return head;
+        }
+        
+        int cnt = 1;
+        Node*temp = head;
+        
+        while(cnt < x-1 && temp -> next != NULL) {
+            cnt++;
+            temp = temp -> next;
+        }
+        
+        // If last node is to be deleted
+        if(temp -> next -> next == NULL) {
+            temp -> next = NULL;
+            return head;
+        }
+        
+        // If node at any other position is to be delted
+        temp -> next = temp -> next -> next;
+        temp -> next -> prev = temp;
+        return head;
+    }
+};
