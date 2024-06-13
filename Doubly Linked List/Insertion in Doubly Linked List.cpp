3cbdb9d68f052d39369fd49ce3674496382f2e3f
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-13 13:46:38 +0530

    Update Insertion in Doubly Linked List.cpp

--- Doubly Linked List/Insertion in Doubly Linked List.cpp
+++ Doubly Linked List/Insertion in Doubly Linked List.cpp
@@ -1,33 +1,33 @@
 void addNode(Node *head, int pos, int data) {
    Node *n = new Node(data);
    
-    // Insertion at first node
-    if(pos == 0) {
-        // If the list is empty
-        if(head == NULL) {
-           head = n;
-           return;
-        }
-        n -> next = head;
-        head -> prev = n;
-       head = n;
-       return;
+   // Insertion at first node
+   if(pos == 0) {
+      // If the list is empty
+      if(head == NULL) {
+         head = n;
+         return;
+      }
+      n -> next = head;
+      head -> prev = n;
+      head = n;
+      return;
     }
    
     int cnt = 0;
     Node *temp = head;
     
     while(cnt < pos && temp -> next != NULL) {
-       temp = temp -> next;
-       cnt++;
+      temp = temp -> next;
+      cnt++;
     }
    
     // Insertion at last node
     if(temp -> next == NULL) {
-        n -> next = NULL;
-        temp -> next = n;
-        n -> prev = temp;
-        return;
+      n -> next = NULL;
+      temp -> next = n;
+      n -> prev = temp;
+      return;
     }
    
     // Insertion in Between two nodes
@@ -36,6 +36,6 @@ void addNode(Node *head, int pos, int data) {
     n -> prev = temp;
     
     if(n -> next != NULL) {
-        n -> next -> prev = n;
+      n -> next -> prev = n;
     }
 }
