a181b4213eb6e75c4a1fac3fbed62dcbc4f54406
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-15 13:45:26 +0530

    Update Odd Even Linked List.cpp

--- Linked List/Odd Even Linked List.cpp
+++ Linked List/Odd Even Linked List.cpp
@@ -1,1 +1,23 @@
+ListNode* oddEvenList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head; // No need to process if the list is empty or has only one
+                     // element.
+    }
 
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+
+    while (even != NULL && even->next != NULL) {
+        odd->next = odd->next->next;
+        even->next = even->next->next;
+
+        odd = odd->next;
+        even = even->next;
+    }
+    odd->next = evenHead;
+    return head;
+}
+
+Input: 1, 2, 3, 4, 5
+Output: 1, 3, 5, 2, 4
