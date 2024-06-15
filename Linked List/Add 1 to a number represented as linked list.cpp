3bdc8d76a390d086450c799686c4892dc44b955f
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-16 00:08:08 +0530

    Create Add 1 to a number represented as linked list.cpp

---
+++ Linked List/Add 1 to a number represented as linked list.cpp
@@ -0,0 +1,26 @@
+Node* addOne(Node *head) {
+  Node* newHead = reverseList(head);
+  Node* temp = newHead;
+        
+  int carry = 1;
+  int sum = 0;
+        
+  while(temp != NULL) {
+    sum = carry + temp->data;
+    temp->data = sum%10;
+    carry = sum/10;
+    temp = temp->next;
+            
+    if(carry == 0) 
+      break;
+  }
+        
+  Node* ans = reverseList(newHead);
+  
+  if(carry) {
+    Node* newNode = new Node(carry);
+    newNode->next = ans;
+    ans = newNode;
+  }        
+  return ans;
+}
