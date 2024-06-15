57691d982d4c434c3016fb5f2e5742e857b55794
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-15 16:51:57 +0530

    Update Remove Nth Node From End of List.cpp

--- Linked List/Remove Nth Node From End of List.cpp
+++ Linked List/Remove Nth Node From End of List.cpp
@@ -1,1 +1,34 @@
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* temp = head;
+    int cnt = 0;
+    while (temp != NULL) {
+        temp = temp->next;
+        cnt++;
+    }
 
+    if (head == NULL || (head->next == NULL && n == 1)) {
+        return NULL;
+    }
+
+    if (cnt == n) {
+        ListNode* del = head;
+        head = head->next;
+        delete del;
+        return head;
+    }
+
+    int res = cnt - n;
+    int k = 1;
+    ListNode* rand = head;
+    while (rand != NULL && k < res) {
+        rand = rand->next;
+        k++;
+    }
+    ListNode* del = rand->next;
+    rand->next = rand->next->next;
+    delete del;
+    return head;
+}
+
+// Input: 1, 2, 3, 4, 5 , n = 2
+// Output: 1, 2, 3, 5
