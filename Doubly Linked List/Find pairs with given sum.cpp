70608c4533051def98f063f070528eb5b63948bd
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-16 14:30:41 +0530

    Update Find pairs with given sum.cpp

--- Doubly Linked List/Find pairs with given sum.cpp
+++ Doubly Linked List/Find pairs with given sum.cpp
@@ -1,1 +1,30 @@
+vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
+  vector<pair<int, int>>ans;
+  Node* left = head;
+  Node* right = head;
+  
+  while(right->next != NULL) {
+    right = right->next;
+  }
+  
+  while(left != NULL && right != NULL && left->data < right->data) {
+    int sum = left->data + right->data;
+    
+    if(sum == target) {
+      ans.push_back({left->data, right->data});
+      left = left->next;
+      right = right->prev;
+    }
+    else if(sum > target) {
+      right = right->prev;
+    }
+    else {
+      left = left->next;
+    }
+  }
+  return ans;
+}
 
+// Input:  1 <-> 2 <-> 4 <-> 5 <-> 6 <-> 8 <-> 9
+// target = 7
+// Output: (1, 6), (2,5)
