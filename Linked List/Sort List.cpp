f9f906950217f869ab11182a2bd418e32e03ca64
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-18 23:03:57 +0530

    Create Sort List.cpp

---
+++ Linked List/Sort List.cpp
@@ -0,0 +1,57 @@
+// Merge Sort on Linked List
+
+ListNode* middleNode(ListNode* head) {
+    ListNode* fast = head;
+    ListNode* slow = head;
+    ListNode* prev = NULL;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        fast = fast->next->next;
+        prev = slow;
+        slow = slow->next;
+    }
+
+    return prev;
+}
+
+ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp = dummy;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val < l2->val) {
+            temp->next = l1;
+            temp = l1;
+            l1 = l1->next;
+        } else {
+            temp->next = l2;
+            temp = l2;
+            l2 = l2->next;
+        }
+    }
+
+    if (l1)
+        temp->next = l1;
+    else
+        temp->next = l2;
+
+    return dummy->next;
+}
+
+ListNode* sortList(ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    ListNode* mid = middleNode(head);
+
+    ListNode* rightHead = mid->next;
+    mid->next = NULL;
+
+    ListNode* leftHead = head;
+
+    leftHead = sortList(leftHead);
+    rightHead = sortList(rightHead);
+
+    return merge2Lists(leftHead, rightHead);
+}
