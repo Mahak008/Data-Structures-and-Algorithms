7843005973b8310729366347f7af48044c9c191c
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-19 18:44:24 +0530

    Update LRU Cache.cpp

--- Doubly Linked List/LRU Cache.cpp
+++ Doubly Linked List/LRU Cache.cpp
@@ -14,7 +14,7 @@ class LRUCache {
         }
     };
 
-    std::unordered_map<int, Node*> mp; // hash map to store key and corresponding node
+    unordered_map<int, Node*> mp; // hash map to store key and corresponding node
     int quantity; // maximum capacity of the cache
     Node* head; // pointer to the head of the doubly linked list
     Node* tail; // pointer to the tail of the doubly linked list
@@ -22,10 +22,13 @@ class LRUCache {
     void removeNode(Node* node) {
         if (node->prev)
             node->prev->next = node->next;
+        
         if (node->next)
             node->next->prev = node->prev;
+        
         if (node == head)
             head = node->next;
+        
         if (node == tail)
             tail = node->prev;
     }
@@ -33,9 +36,12 @@ class LRUCache {
     void addToFront(Node* node) {
         node->next = head;
         node->prev = nullptr;
+        
         if (head)
             head->prev = node;
+        
         head = node;
+        
         if (!tail)
             tail = head;
     }
@@ -46,7 +52,11 @@ class LRUCache {
     }
 
 public:
-    LRUCache(int capacity) : quantity(capacity), head(nullptr), tail(nullptr) {}
+    LRUCache(int capacity) {
+        quantity = capacity;
+        head = NULL;
+        tail = NULL;
+    }
 
     int get(int key) {
         if (mp.find(key) != mp.end()) {
@@ -62,17 +72,26 @@ public:
             Node* node = mp[key];
             node->value = value;
             moveToFront(node);
-        } else {
+        } 
+        else {
             Node* newNode = new Node(key, value);
+            
             if (mp.size() == quantity) {
                 mp.erase(tail->key);
                 Node* oldTail = tail;
                 removeNode(oldTail);
                 delete oldTail;
             }
+            
             addToFront(newNode);
             mp[key] = newNode;
         }
     }
 };
 
+// Input:
+// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+
+// Output:
+// [null, null, null, 1, null, -1, null, -1, 3, 4]
