1b063def0da9010226888c00e6d9c3d27d95b679
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-24 22:05:14 +0530

    Update Binary Search.cpp

--- Recursion/Binary Search.cpp
+++ Recursion/Binary Search.cpp
@@ -1,18 +1,23 @@
-bool b_search(int *arr, start, end, k) {
+int b_search(vector<int> &arr, int start, int end, int k) {
   if(start > end) {
-    return false;
+    return -1;
   }
 
   int mid = (start + end)/2;
   
   if(arr[mid] == k) {
-    return true;
+    return mid;
   }
-  else if(arr[mid] < k) {
+  else if(arr[mid] > k) {
     return b_search(arr, start, mid-1, k);
   }
   else {
     return b_search(arr, mid+1, end, k);
   }
-  return false;
+  return -1;
+}
+
+int search(vector<int> &nums, int target) {
+    int ans = b_search(nums, 0, nums.size()-1, target);
+    return ans;
 }
