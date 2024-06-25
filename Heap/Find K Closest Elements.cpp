e9101d57d875995f6f5e23c681970eb4605364af
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-25 15:45:46 +0530

    Update Find K Closest Elements.cpp

--- Heap/Find K Closest Elements.cpp
+++ Heap/Find K Closest Elements.cpp
@@ -1,1 +1,19 @@
+typedef pair<int, int> P;
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    priority_queue<P, vector<P>, greater<P>> pq;
+    vector<int> ans;
 
+    for (int i = 0; i < arr.size(); i++) {
+        pq.push(make_pair(abs(arr[i] - x), arr[i]));
+    }
+    while (k--) {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+// Output: [1,2,3,4]
