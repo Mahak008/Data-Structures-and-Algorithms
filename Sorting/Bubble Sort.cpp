ecf664fcc0ce8ffc9113d7ccf8a791682a838510
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-25 12:47:09 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -14,6 +14,21 @@ for(int i = n-1; i >= 0; i--) {
   }
 }
 
+// Recursion
+void bubbleSort(int arr[], int n) {
+  if(n == 0 || n == 1) {
+    return;
+  }
+  
+  for(int i = 0; i < n-1; i++) {
+    if(arr[i] > arr[i+1]) {
+      swap(arr[i], arr[i+1]);
+    }
+  }
+        
+  bubbleSort(arr, n-1);
+}
+
 // Output after each Iteration:
 // 13 24 46 52 20 9 
 // 13 24 46 20 52 9 
