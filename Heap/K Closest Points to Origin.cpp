e875d9c8ae1659e191dec2e9b7c0de9dc9245684
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-26 15:29:09 +0530

    Create K Closest Points to Origin.cpp

---
+++ Heap/K Closest Points to Origin.cpp
@@ -0,0 +1,24 @@
+typedef pair<double, pair<int, int>> p;
+vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    priority_queue<p, vector<p>, greater<p>> pq;
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < points.size(); i++) {
+
+        int num1 = points[i][0];
+        int num2 = points[i][1];
+        int sum = num1 * num1 + num2 * num2;
+
+        pq.push(make_pair(sqrt(sum), make_pair(num1, num2)));
+    }
+
+    while (k--) {
+        ans.push_back({pq.top().second.first, pq.top().second.second});
+        pq.pop();
+    }
+    return ans;
+}
+
+// Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+// Output: [[3,3],[-2,4]]
+// Explanation: The answer [[-2,4],[3,3]] would also be accepted.
