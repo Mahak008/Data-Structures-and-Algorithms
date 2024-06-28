9725b9fdebe13d73819d7a6ac7773e256ee732e9
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-28 14:11:35 +0530

    Create K-th Symbol in Grammar.cpp

---
+++ Recursion/K-th Symbol in Grammar.cpp
@@ -0,0 +1,22 @@
+int kthGrammar(int n, int k) {
+    int len = pow(2, n - 1);
+    int mid = len / 2;
+    int ans;
+
+    if (n == 1 && k == 1) {
+        return 0;
+    }
+
+    if (k <= mid) {
+        ans = kthGrammar(n - 1, k);
+    } else {
+        ans = !(kthGrammar(n - 1, k - mid));
+    }
+    return ans;
+}
+
+// 0
+// 0 1
+// 0 1 1 0
+// 0 1 1 0 1 0 0 1
+// 0 1 1 0 1 0 0 1 1 0 0 1 0 1 1 0
