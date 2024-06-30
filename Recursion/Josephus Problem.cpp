8751682950cdfd0dcf110d39adcef0465ec48983
Mahak Garg <mahakgarg8002@gmail.com>
2024-06-30 14:06:54 +0530

    Update Josephus Problem.cpp

--- Recursion/Josephus Problem.cpp
+++ Recursion/Josephus Problem.cpp
@@ -1,5 +1,31 @@
 // Inductive Base Hypothesis
 
+void solve(vector<int> v, int k, int index, int& ans) {
+    if (v.size() == 1) {
+        ans = v[0];
+        return;
+    }
+    index = (index + k) % v.size();
+    v.erase(v.begin() + index);
+    solve(v, k, index, ans);
+}
+
+int findTheWinner(int n, int k) {
+    vector<int> v;
+    for (int i = 1; i <= n; i++) {
+        v.push_back(i);
+    }
+
+    int index = 0;
+    int ans = -1;
+    k = k - 1;
+
+    solve(v, k, index, ans);
+    return ans;
+}
+
+// Alternate Approach
+
 int solve(int n, int k) {
     // Base case: when only one person is left
     if (n == 1) {
