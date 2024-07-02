6b90441453c4ae3a1ec371f47af4b40503c2aaf4
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-02 19:43:33 +0530

    Create Binary strings with no consecutive 1s..cpp

---
+++ Recursion/Binary strings with no consecutive 1s..cpp
@@ -0,0 +1,22 @@
+void solve(int n, string output, vector<string> &ans) {
+  if (n == 0) {
+    ans.push_back(output);
+    return;
+  }
+
+  solve(n - 1, output, ans);
+
+  if (output.empty() || output.back() == '0') {
+    solve(n - 1, output, ans);
+  }
+}
+
+vector<string> generateString(int N) {
+  vector<string> ans;
+  string output = "";
+  solve(N, output, ans);
+  return ans;
+}
+
+// Input: 4
+// Output: 0000 0001 0010 0100 0101 1000 1001 1010 
