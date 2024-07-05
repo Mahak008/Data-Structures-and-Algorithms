27297a5ae1bffeb013e415dc7bc07e39d9a0acaa
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-05 19:13:24 +0530

    Update Rat in a Maze.cpp

--- Backtracking/Rat in a Maze.cpp
+++ Backtracking/Rat in a Maze.cpp
@@ -46,6 +46,57 @@ vector<string> findPath(vector<vector<int>> &m, int n) {
   return ans;
 }
 
+
+// Alternate Method
+
+
+struct Direction {
+    pair<int, int> move;
+    char symbol;
+};
+
+void solve(int row, int col, vector<vector<int>> &m, int n, 
+  vector<vector<int>> visited, string move, vector<string>&ans, 
+  vector<Direction>& dir) {
+    
+  if(row == n-1 && col == n - 1) {
+    ans.push_back(move);
+    return;
+  }    
+        
+  visited[row][col] = 1;
+        
+  for (auto& d : dir) {
+    int i = row + d.move.first;
+    int j = col + d.move.second;
+
+    if (i >= 0 && i < n && j >= 0 && j < n && m[i][j] == 1 && !visited[i][j]) {
+      solve(i, j, m, n, visited, move + d.symbol, ans, dir);
+    }
+  }
+        
+  visited[row][col] = 0;
+}
+    
+vector<string> findPath(vector<vector<int>> &m, int n) {
+  vector<string>ans;
+  vector<vector<int>> visited(n, vector<int>(n, 0
+  int row = 0, col = 0;
+        
+  vector<Direction> dir = {
+    {{1, 0}, 'D'}, // Down
+    {{0, -1}, 'L'}, // Left
+    {{0, 1}, 'R'}, // Right
+    {{-1, 0}, 'U'} // Up
+  };
+        
+  if(m[0][0] == 1) {
+    solve(row, col, m, n, visited, move, ans, dir);
+  }
+        
+  return ans;
+}
+
 // Input: N = 4
 // m[][] = {{1, 0, 0, 0},
 //          {1, 1, 0, 1}, 
