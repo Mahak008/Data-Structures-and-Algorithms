91c0e51ac397b85f87311d079f83587cd700b712
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-05 18:58:57 +0530

    Update Word Search.cpp

--- Backtracking/Word Search.cpp
+++ Backtracking/Word Search.cpp
@@ -1,3 +1,29 @@
+bool find(vector<vector<char>>& board, string word, int i, int j, int index,
+          vector<vector<int>>& visited, vector<pair<int, int>>& dir) {
+    if (index == word.length()) {
+        return true;
+    }
+
+    if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
+        visited[i][j] || board[i][j] != word[index]) {
+        return false;
+    }
+
+    visited[i][j] = 1;
+
+    for (auto d : dir) {
+        int row = i + d.first;
+        int col = j + d.second;
+
+        if (find(board, word, row, col, index + 1, visited, dir)) {
+            return true;
+        }
+    }
+
+    visited[i][j] = 0;
+    return false;
+}
+
 bool exist(vector<vector<char>>& board, string word) {
     int n = board.size();
     int m = board[0].size();
