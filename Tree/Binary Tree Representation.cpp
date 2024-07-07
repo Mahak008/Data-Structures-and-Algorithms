2f016f8f1a3af8e00c41cfbe5b44567218fef9db
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-07 17:05:51 +0530

    Create Binary Tree Representation.cpp

---
+++ Tree/Binary Tree Representation.cpp
@@ -0,0 +1,24 @@
+// Iterative Approach
+
+void create_tree(node* root, vector<int>& vec) {
+    queue<node*> q;
+    q.push(root);
+    int i = 0; // Start from the second element as the first is root
+
+    while (!q.empty() && i < vec.size()) {
+        node* current = q.front();
+        q.pop();
+
+        // Left child
+        if (++i < vec.size()) {
+            current->left = newNode(vec[i]);
+            q.push(current->left);
+        }
+
+        // Right child
+        if (++i < vec.size()) {
+            current->right = newNode(vec[i]);
+            q.push(current->right);
+        }
+    }
+}
