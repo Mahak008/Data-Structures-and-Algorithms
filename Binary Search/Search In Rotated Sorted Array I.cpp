3d4bc9db6fe761b32a7dbdd72481e025cd8ad066
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-09 12:14:35 +0530

    Update Search In Rotated Sorted Array I.cpp

--- Binary Search/Search In Rotated Sorted Array I.cpp
+++ Binary Search/Search In Rotated Sorted Array I.cpp
@@ -2,15 +2,20 @@ int search(vector<int> &arr, int n, int k) {
   int low = 0, high = n - 1;
   while (low <= high) {
     int mid = (low + high) / 2;
+    
     if (arr[mid] == k) {
       return mid;
-    } else if (arr[low] <= arr[mid]) {
+    } 
+    
+    else if (arr[low] <= arr[mid]) {
       if (arr[low] <= k && k <= arr[mid]) {
         high = mid - 1;
       } else {
         low = mid + 1;
       }
-    } else if (arr[mid] <= arr[high]) {
+    } 
+    
+    else if (arr[mid] <= arr[high]) {
       if (arr[mid] <= k && k <= arr[high]) {
         low = mid + 1;
       } else {
