e6e087852d215a047f1bef11877d821483cc9cf0
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-10 15:18:10 +0530

    Update Top View.cpp

--- Binary Tree/Top View.cpp
+++ Binary Tree/Top View.cpp
@@ -1,4 +1,40 @@
+vector<int> topView(Node *root) {
+    map<int, int> nodes; // vertical line, node
+    queue<pair<Node*, int>> q; // node, level
+    vector<int> ans;
+    
+    if(root == NULL) {
+        return ans;
+    }
 
+    q.push({root, 0});
+    
+    while(!q.empty()) {
+        auto temp = q.front();
+        q.pop();
+        
+        Node* node = temp.first;
+        int x = temp.second;
+        
+        if (nodes.find(x) == nodes.end()) {
+            nodes[x] = node->data;
+        }
+        
+        if(node->left) {
+            q.push({node->left, x-1});
+        }
+        
+        if(node->right) {
+            q.push({node->right, x+1});
+        }
+    }
+    
+    for(auto it: nodes) {
+        ans.push_back(it.second);
+    }
+    
+    return ans;
+}
 
 // Input:
 //        10
