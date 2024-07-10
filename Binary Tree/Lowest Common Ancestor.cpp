1ae968aed6591d7a1ccd6a1fb76194b89b02bbcc
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-10 18:32:59 +0530

    Create Lowest Common Ancestor.cpp

---
+++ Binary Tree/Lowest Common Ancestor.cpp
@@ -0,0 +1,20 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == NULL || root == p || root == q) {
+        return root;
+    }
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (left == NULL) {
+        return right;
+    } else if (right == NULL) {
+        return left;
+    } else {
+        return root;
+    }
+}
+
+// Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+// Output: 3
+// Explanation: The LCA of nodes 5 and 1 is 3.
