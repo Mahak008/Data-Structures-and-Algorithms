462ad62ef7e9a7de7baea4b4f818408ca2147977
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-10 16:13:40 +0530

    Update Symmetric Tree.cpp

--- Binary Tree/Symmetric Tree.cpp
+++ Binary Tree/Symmetric Tree.cpp
@@ -1,1 +1,21 @@
 // Leetcode - 101
+
+bool solve(TreeNode* left, TreeNode* right) {
+    if (left == NULL || right == NULL) {
+        return left == right;
+    }
+
+    if (left->val != right->val) {
+        return false;
+    }
+
+    return (solve(left->left, right->right)) &&
+           (solve(left->right, right->left));
+}
+
+bool isSymmetric(TreeNode* root) {
+    return (root == NULL) || (solve(root->left, root->right));
+}
+
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
