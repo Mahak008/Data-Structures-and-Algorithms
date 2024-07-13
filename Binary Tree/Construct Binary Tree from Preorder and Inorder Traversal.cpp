26226f5d7dc4307073f2defe3c833acbf8b2a5a7
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-13 06:52:53 +0530

    Create Construct Binary Tree from Preorder and Inorder Traversal.cpp

---
+++ Binary Tree/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,32 @@
+// Leetcode - 105 
+
+TreeNode* findTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int, int>& mp) {
+    if (preStart > preEnd || inStart > inEnd)
+        return NULL;
+
+    TreeNode* root = new TreeNode(preorder[preStart]);
+
+    int inRoot = mp[root->val];
+
+    int numsLeft = inRoot - inStart;
+
+    root->left = findTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, mp);
+    root->right = findTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, mp);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    map<int, int> mp;
+
+    for (int i = 0; i < inorder.size(); i++) {
+        mp[inorder[i]] = i;
+    }
+
+    TreeNode* root = findTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, mp);
+
+    return root;
+}
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
