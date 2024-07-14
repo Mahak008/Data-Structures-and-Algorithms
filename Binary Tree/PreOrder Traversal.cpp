e6d0aa9411b87f5565f881e2b7845111dcf628cb
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-14 17:09:04 +0530

    Update PreOrder Traversal.cpp

--- Binary Tree/PreOrder Traversal.cpp
+++ Binary Tree/PreOrder Traversal.cpp
@@ -49,6 +49,31 @@ vector<int> preorderTraversal(TreeNode* root) {
 
 // Morris Traversal
 
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> preorder;
+    TreeNode* cur = root;
+
+    while (cur != NULL) {
+        if (!cur->left) {
+            preorder.push_back(cur->val);
+            cur = cur->right;
+        } else {
+            TreeNode* prev = cur->left;
+            while (prev->right && prev->right != cur) {
+                prev = prev->right;
+            }
+            if (prev->right == NULL) {
+                prev->right = cur;
+                preorder.push_back(cur->val);
+                cur = cur->left;
+            } else {
+                prev->right = NULL;
+                cur = cur->right;
+            }
+        }
+    }
+    return preorder;
+}
 
 // Input
 //          1
