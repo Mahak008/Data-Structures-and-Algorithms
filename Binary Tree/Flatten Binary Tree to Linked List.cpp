365515785bb8655f7e1619267e36508d333984be
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-16 18:16:52 +0530

    Update Flatten Binary Tree to Linked List.cpp

--- Binary Tree/Flatten Binary Tree to Linked List.cpp
+++ Binary Tree/Flatten Binary Tree to Linked List.cpp
@@ -17,6 +17,34 @@ public:
     }
 };
 
+// Using Stack
+void flatten(TreeNode* root) {
+    if (!root)
+        return;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while (!st.empty()) {
+        TreeNode* curr = st.top();
+        st.pop();
+
+        if (curr->right) {
+            st.push(curr->right);
+        }
+
+        if (curr->left) {
+            st.push(curr->left);
+        }
+
+        if (!st.empty()) {
+            curr->right = st.top();
+        }
+
+        curr->left = NULL;
+    }
+}
+
 // Morris Traversal Approach
 
 void flatten(TreeNode* root) {
