43abe489a63bb3f71a1a53ab89ab808bd67a6724
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-17 14:23:55 +0530

    Update Kth Smallest Element.cpp

--- Binary Search Tree/Kth Smallest Element.cpp
+++ Binary Search Tree/Kth Smallest Element.cpp
@@ -1,2 +1,50 @@
 // Leetcode - 230
 
+void morrisTraversal(TreeNode* root, int& k, int& ans) {
+    if (!root)
+        return;
+
+    TreeNode* curr = root;
+    int cnt = 0;
+
+    while (curr) {
+        if (!curr->left) {
+            ++cnt;
+
+            if (cnt == k) {
+                ans = curr->val;
+            }
+
+            curr = curr->right;
+        } else {
+            TreeNode* prev = curr->left;
+
+            while (prev->right && prev->right != curr) {
+                prev = prev->right;
+            }
+
+            if (prev->right == NULL) {
+                prev->right = curr;
+                curr = curr->left;
+            } else {
+                prev->right = NULL;
+                ++cnt;
+
+                if (cnt == k) {
+                    ans = curr->val;
+                }
+
+                curr = curr->right;
+            }
+        }
+    }
+}
+
+int kthSmallest(TreeNode* root, int k) {
+    int ans = 0;
+    morrisTraversal(root, k, ans);
+    return ans;
+}
+
+// Input: root = [3,1,4,null,2], k = 1
+// Output: 1
