82b642160d733b5db7c7fe568e68f6cb29310e11
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-17 11:15:00 +0530

    Create LCA.cpp

---
+++ Binary Search Tree/LCA.cpp
@@ -0,0 +1,14 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root->val > p->val && root->val > q->val) {
+        return lowestCommonAncestor(root->left, p, q);
+    }
+
+    if (root->val < p->val && root->val < q->val) {
+        return lowestCommonAncestor(root->right, p, q);
+    }
+
+    return root;
+}
+
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+// Output: 2
