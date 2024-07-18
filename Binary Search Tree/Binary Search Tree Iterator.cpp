f82fe36f07dc9ebdbc4f61fd936c5bda25faddbf
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-18 18:56:31 +0530

    Create Binary Search Tree Iterator.cpp

---
+++ Binary Search Tree/Binary Search Tree Iterator.cpp
@@ -0,0 +1,32 @@
+// Leetcode - 173
+
+class BSTIterator {
+    stack<TreeNode*>st;
+    void pushAll(TreeNode* root) {
+        while(root) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        pushAll(temp->right);
+        return temp->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
+// Input
+// ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+// [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+  
+// Output - [null, 3, 7, true, 9, true, 15, true, 20, false]
