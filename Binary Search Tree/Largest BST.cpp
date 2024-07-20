005d194fc05d79524499da3622ab7946095d9de9
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-20 12:56:07 +0530

    Create Largest BST.cpp

---
+++ Binary Search Tree/Largest BST.cpp
@@ -0,0 +1,11 @@
+
+
+// Input:    6
+//         /   \
+//       6      2              
+//        \    / \
+//         2  1   3
+// Output: 3
+// Explanation: The following sub-tree is a BST of size 3:  2
+//                                                        /   \
+//                                                       1     3
