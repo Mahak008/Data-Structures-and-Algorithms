679b28e5c9d60dc64cbaa1d3d096092855bbf7a3
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-20 16:11:14 +0530

    Update Largest BST.cpp

--- Binary Search Tree/Largest BST.cpp
+++ Binary Search Tree/Largest BST.cpp
@@ -1,4 +1,35 @@
+class NodeValue {
+    public: 
+    
+    int minVal, maxVal, maxSize;
+    
+    NodeValue(int minVal, int maxVal, int maxSize) {
+        this->minVal = minVal;
+        this->maxVal = maxVal;
+        this->maxSize = maxSize;
+    }
+};
 
+class Solution{
+    private:
+    NodeValue solve(Node* root) {
+        if(!root) return NodeValue(INT_MAX, INT_MIN, 0);
+    
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+    
+        if(left.maxVal < root->data && root->data < right.minVal) {
+            return NodeValue(min(root->data,left.minVal), max(root->data, right.maxVal), 1 + left.maxSize + right.maxSize);
+        }
+    
+        return NodeValue(INT_MIN, INT_MAX, max(left.maxSize, right. maxSize));
+    }
+
+    public:
+    int largestBst(Node *root) {
+        return solve(root).maxSize;
+    }
+};
 
 // Input:    6
 //         /   \
