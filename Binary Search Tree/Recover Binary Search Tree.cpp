4a0e9b49af62b229953d72ad23a9e388ca8c4034
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-20 15:18:30 +0530

    Update Recover Binary Search Tree.cpp

--- Binary Search Tree/Recover Binary Search Tree.cpp
+++ Binary Search Tree/Recover Binary Search Tree.cpp
@@ -1,6 +1,45 @@
 // Leetcode - 99
 
+class Solution {
+private:
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
 
+    void inorder(TreeNode* root) {
+        if (!root)
+            return;
+
+        inorder(root->left);
+
+        if (prev != NULL && root->val < prev->val) {
+            if (!first) {
+                first = prev;
+                middle = root;
+            } else {
+                last = root;
+            }
+        }
+
+        prev = root;
+        inorder(root->right);
+    }
+
+public:
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+
+        inorder(root);
+
+        if (first && last) {
+            swap(first->val, last->val);
+        } else if (first && middle) {
+            swap(first->val, middle->val);
+        }
+    }
+};
 
 // Input: root = [1,3,null,null,2]
 // Output: [3,1,null,null,2]
