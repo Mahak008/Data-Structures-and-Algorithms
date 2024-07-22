01d6bddc55dc96a0678058756b4daabc534a8f2f
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-22 09:33:44 +0530

    Update Range Sum of BST.cpp

--- Binary Search Tree/Range Sum of BST.cpp
+++ Binary Search Tree/Range Sum of BST.cpp
@@ -36,6 +36,26 @@ int rangeSumBST(TreeNode* root, int low, int high) {
     return res;
 }
 
+// Recursive Approach
+
+int rangeSumBST(TreeNode* root, int low, int high) {
+    if (!root) {
+        return 0;
+    }
+
+    if (root->val >= low && root->val <= high) {
+        return root->val + rangeSumBST(root->left, low, high) +
+               rangeSumBST(root->right, low, high);
+    }
+
+    if (root->val < low) {
+        return rangeSumBST(root->right, low, high);
+    }
+
+    // if(root->val > high)
+    return rangeSumBST(root->left, low, high);
+}
+
 // Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
 // Output: 32
 // Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
