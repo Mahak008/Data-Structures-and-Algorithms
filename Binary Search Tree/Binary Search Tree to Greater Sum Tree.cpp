0e921c8d85a4301010397ac7d6ede86b5fa21250
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-25 19:58:49 +0530

    Update Binary Search Tree to Greater Sum Tree.cpp

--- Binary Search Tree/Binary Search Tree to Greater Sum Tree.cpp
+++ Binary Search Tree/Binary Search Tree to Greater Sum Tree.cpp
@@ -1,6 +1,21 @@
 // Leetcode - 1038
 
+void solve(TreeNode* root, int& sum) {
+    if (!root) {
+        return;
+    }
 
+    solve(root->right, sum);
+    sum += root->val;
+    root->val = sum;
+    solve(root->left, sum);
+}
+
+TreeNode* bstToGst(TreeNode* root) {
+    int sum = 0;
+    solve(root, sum);
+    return root;
+}
 
 //          4
 //       /    \
