e82acbb6c83690d666727a5254d22b89478d0e83
Mahak Garg <mahakgarg8002@gmail.com>
2024-07-30 13:50:29 +0530

    Update Minimum Deletions to Make String Balanced.cpp

--- Stack/Minimum Deletions to Make String Balanced.cpp
+++ Stack/Minimum Deletions to Make String Balanced.cpp
@@ -1,5 +1,22 @@
 // Leetcode - 1653
 
+int minimumDeletions(string s) {
+    int n = s.length();
+    int cnt = 0;
+    stack<char> st;
+
+    for (int i = 0; i < n; i++) {
+        if (!st.empty() && s[i] == 'a' && st.top() == 'b') {
+            st.pop();
+            cnt++;
+        } else {
+            st.push(s[i]);
+        }
+    }
+
+    return cnt;
+}
+
 // Input: s = "aababbab"
 // Output: 2
 // Explanation: You can either:
