5dc3323385c7a23d801c1f32f6d303bc1a452567
Mahak Garg <mahakgarg8002@gmail.com>
2024-08-02 07:52:48 +0530

    Update Minimum Swaps to Group All 1's Together II.cpp

--- Arrays/Minimum Swaps to Group All 1's Together II.cpp
+++ Arrays/Minimum Swaps to Group All 1's Together II.cpp
@@ -1,6 +1,49 @@
 // Leetcode - 2134
 
+int minSwaps(vector<int>& nums) {
+    int n = nums.size();
+    int onesCount = 0;
 
+    // Count total number of 1's in the array
+    for (int num : nums) {
+        if (num == 1) {
+            onesCount++;
+        }
+    }
+
+    if (onesCount == 0)
+        return 0; // No 1's to group
+
+    // Extend the array to handle circular nature
+    vector<int> extended(nums.begin(), nums.end());
+    extended.insert(extended.end(), nums.begin(), nums.end());
+
+    // Find the minimum number of 0's in a window of size `onesCount`
+    int minZeros = onesCount;
+    int currentZeros = 0;
+
+    // Initial window of size `onesCount`
+    for (int i = 0; i < onesCount; ++i) {
+        if (extended[i] == 0) {
+            currentZeros++;
+        }
+    }
+
+    minZeros = min(minZeros, currentZeros);
+
+    // Slide the window
+    for (int i = onesCount; i < extended.size(); ++i) {
+        if (extended[i] == 0) {
+            currentZeros++;
+        }
+        if (extended[i - onesCount] == 0) {
+            currentZeros--;
+        }
+        minZeros = min(minZeros, currentZeros);
+    }
+
+    return minZeros;
+}
 
 // Input: nums = [0,1,0,1,1,0,0]
 // Output: 1
