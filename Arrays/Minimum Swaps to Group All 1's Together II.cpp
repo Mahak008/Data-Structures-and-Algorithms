748d5d72708a5adda6f0cdf2ce00ba054c44ccc9
Mahak Garg <mahakgarg8002@gmail.com>
2024-08-02 07:41:15 +0530

    Create Minimum Swaps to Group All 1's Together II.cpp

---
+++ Arrays/Minimum Swaps to Group All 1's Together II.cpp
@@ -0,0 +1,10 @@
+// Leetcode - 2134
+
+
+
+// Input: nums = [0,1,0,1,1,0,0]
+// Output: 1
+// Explanation: Here are a few of the ways to group all the 1's together:
+// [0,0,1,1,1,0,0] using 1 swap.
+// [0,1,1,1,0,0,0] using 1 swap.
+// [1,1,0,0,0,0,1] using 2 swaps (using the circular property of the array).
