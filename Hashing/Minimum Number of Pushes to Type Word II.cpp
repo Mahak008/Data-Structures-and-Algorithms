ca59c78d4b802819fba6a5ce1f0023598e83020b
Mahak Garg <mahakgarg8002@gmail.com>
2024-08-06 15:33:35 +0530

    Create Minimum Number of Pushes to Type Word II.cpp

---
+++ Hashing/Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,14 @@
+// Leetcode - 3016
+
+
+
+// Input: word = "xyzxyzxyzxyz"
+// Output: 12
+  
+// Explanation: The remapped keypad given in the image provides the minimum cost.
+// "x" -> one push on key 2
+// "y" -> one push on key 3
+// "z" -> one push on key 4
+// Total cost is 1 * 4 + 1 * 4 + 1 * 4 = 12
+// It can be shown that no other mapping can provide a lower cost.
+// Note that the key 9 is not mapped to any letter: it is not necessary to map letters to every key, but to map all the letters.
