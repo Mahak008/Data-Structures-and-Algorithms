4249e4de5db32f709cc3a554a16ea950649cf3d3
Mahak Garg <mahakgarg8002@gmail.com>
2024-08-21 22:16:28 +0530

    Update Jump Game II.cpp

--- Greedy Algorithm/Jump Game II.cpp
+++ Greedy Algorithm/Jump Game II.cpp
@@ -1,5 +1,36 @@
 // Leetcode - 45
 
+int solve(vector<int>& nums, int idx) {
+    int n = nums.size();
+
+    if (n <= 1) {
+        return 0;
+    }
+
+    if (idx >= n - 1) {
+        return 0;
+    }
+
+    int mini = INT_MAX;
+
+    for (int i = 1; i <= nums[idx]; i++) {
+        if (idx + i < n) { // Check to ensure we are within bounds
+            int jumps = solve(nums, idx + i);
+            if (jumps != INT_MAX) {
+                mini = min(mini, jumps + 1);
+            }
+        }
+    }
+
+    return mini;
+}
+
+int jump(vector<int>& nums) { 
+  return solve(nums, 0); 
+}
+
+// Time Limit Exceeded
+
 
 
 // Input: nums = [2,3,1,1,4]
