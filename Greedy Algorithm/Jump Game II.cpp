50ce6bba44c2a8618b38715314ea561fd609c800
Mahak Garg <mahakgarg8002@gmail.com>
2024-08-22 08:23:12 +0530

    Update Jump Game II.cpp

--- Greedy Algorithm/Jump Game II.cpp
+++ Greedy Algorithm/Jump Game II.cpp
@@ -31,7 +31,21 @@ int jump(vector<int>& nums) {
 
 // Time Limit Exceeded
 
-
+// Approach - check in range
+int jump(vector<int>& nums) {
+    int n = nums.size();
+    int j = 0, l = 0, r = 0;
+    while (r < n - 1) {
+        int farthest = 0;
+        for (int i = l; i <= r; i++) {
+            farthest = max(farthest, i + nums[i]);
+        }
+        l = r + 1;
+        r = farthest;
+        j++;
+    }
+    return j;
+}
 
 // Input: nums = [2,3,1,1,4]
 // Output: 2
