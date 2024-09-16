8df62e1bf87aa257a5446c4d8437a66be2398427
Mahak Garg <mahakgarg8002@gmail.com>
2024-09-16 14:40:57 +0530

    Create Longest valid Parentheses.cpp

---
+++ Stack/Longest valid Parentheses.cpp
@@ -0,0 +1,25 @@
+int maxLength(string str) {
+  int n = str.length();
+  stack<int>st;
+  st.push(-1);
+  int maxLen = 0;
+        
+  for(int i = 0; i < n; i++) {
+    if(str[i] == '(') {
+      st.push(i);
+    } else {
+      st.pop();
+                
+      if(!st.empty()) {
+        maxLen = max(maxLen, i - st.top());
+      } else {
+        st.push(i);
+      }
+    }
+  }
+        
+  return maxLen;
+}
+
+// Input: str = )()())
+// Output: 4
