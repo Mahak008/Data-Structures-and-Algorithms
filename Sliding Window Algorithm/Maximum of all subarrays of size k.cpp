0ccb2f51e8cc68dd58795fbff6133bc70ffea393
Mahak Garg <mahakgarg8002@gmail.com>
2024-09-27 09:53:07 +0530

    Update Maximum of all subarrays of size k.cpp

--- Sliding Window Algorithm/Maximum of all subarrays of size k.cpp
+++ Sliding Window Algorithm/Maximum of all subarrays of size k.cpp
@@ -2,17 +2,23 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k, int n) {
   deque<int> dq;
   vector<int> ans;
   int r = 0;
+  
   while(r < n) {
     if (!dq.empty() && dq.front() == (r-k)) {
       dq.pop_front();
     }
+  
     while (!dq.empty() && nums[dq.back()] < nums[r]) {
       dq.pop_back();
     }
+    
     dq.push_back(r);
+    
     if (r >= (k-1)) {
       ans.push_back(nums[dq.front()]);
     }
+
+    ++r;
   }  
   return ans;
 }
